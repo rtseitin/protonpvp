35f75e2ba0a9c63541adf7085d41f523a0d563ec
rtseitin <robert.tseitin.official@gmail.com>
2023-01-09 23:11:30 -0500

    Added dotenv to support .env files in dev env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+require("dotenv").config()
 import { DatabaseConfiguration, ServerConfiguration } from './config';
 import { DatabaseService } from './services/Database';
 import { ServerService } from './services/Server';
