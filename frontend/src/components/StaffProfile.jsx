7c5788c95614af21c2a9c6cdf2b90f50f00863d3
rtseitin <robert.tseitin.official@gmail.com>
2023-01-09 23:11:01 -0500

    Changed class to function components

--- frontend/src/components/StaffProfile.jsx
+++ frontend/src/components/StaffProfile.jsx
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { useState } from 'react';
 
-class StaffProfile extends Component {
-    render() {
-        return this.props.profiles.map(staff => (
-            <React.Fragment>
-                <div style={{justifyContent: 'center', alignItems: 'center', display: 'inline-block', paddingRight: '30px'}}>
-                    <img
-                        alt='BizarreAvatar'
-                        src={`https://cravatar.eu/avatar/${staff.uuid}/100.png`}
-                        width='125px'
-                    />
-                    <p style={{fontSize: '14px', fontWeight: '700'}}>{staff.name}</p>
-                </div>
-            </React.Fragment>
-        ))
-    }
+export default function StaffProfile({ profiles }) {
+  const [clicked, setClicked] = useState(false);
+
+  const buttonClick = () => {
+    setClicked(!clicked);
+  }
+
+  return profiles.map((staff) => (
+    <>
+      <div
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "inline-block",
+          paddingRight: "30px",
+        }}
+        onClick={buttonClick}
+      >
+        {clicked && <p>{staff.name}</p>}
+        <img
+          alt={`${staff.name} Minecraft Skin Avatar`}
+          src={`https://cravatar.eu/avatar/${staff.uuid}/100.png`}
+          width="125px"
+        />
+        <p style={{ fontSize: "14px", fontWeight: "700" }}>{staff.name}</p>
+      </div>
+    </>
+  ));
 }
-
 
 StaffProfile.propType = {
-    profiles: PropTypes.array.isRequired
-}
-
-export default StaffProfile;
+  profiles: PropTypes.array.isRequired,
+};
