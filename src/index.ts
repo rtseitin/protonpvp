d86439448121dd80e49db2ae9c6f847269d9f2e7
rtseitin <robert.tseitin.official@gmail.com>
2023-01-10 00:05:01 -0500

    Original content deleted, cloned to backend folder

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { DatabaseConfiguration, ServerConfiguration } from './config';
-import { DatabaseService } from './services/Database';
-import { ServerService } from './services/Server';
-
-new DatabaseService(DatabaseConfiguration);
-new ServerService(ServerConfiguration);
--- src/interfaces/rank.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface IRank extends Document {
-    name: string;
-    priority: number;
-}
